4eb7a9e1fbc69b2f79104ddadda6998a39509ff6
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-08-21 23:26:42 -0300

    Adição: aqui foi adicionada a lógica da tag canvas para criar um arco na primeira seção da página

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,3 +21,32 @@ var Card = {
 
     }
 };
+
+
+let screen = document.getElementById("screen");
+let context = screen.getContext("2d");
+
+let x = 150
+let y = 300
+let radius = 350
+let begin = 0
+let end = 2 * Math.PI
+
+context.beginPath() //define um novo caminho
+
+context.arc(x, y, radius, begin, end)
+
+context.strokeStyle = "#212121";
+context.fillStyle = "#fff";
+context.fill()
+/*
+argumentos: 
+    1º - distância da borda (horizontal);
+    2º - distância da borda vertical;
+    3º - raio;
+    4º - ângulo que ele começa;
+    5º - ângulo que ele termina.
+    OBS: o começo e fim (4º e 5º) são medidas em randianos
+ */
+
+context.stroke() //diz que estamos de fato desenhando.
