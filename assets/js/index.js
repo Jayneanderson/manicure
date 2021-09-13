f207a915150d999b9a77c029d7b40a81306a993b
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-09-12 22:54:11 -0300

    Atualização: aqui eu retirei os códigos e comentários desnecessários e realizei testes nas funções de abrir e fechar card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,16 @@
-// //O que eu preciso fazer?
-// //quando estiver com 
-
-
-
-
-// var Card = {
-//     open() {
-//         const getIcon = document.querySelector(".contact-icon")
-//         var getCard = document.querySelector("#contact")
+var Card = {
+    open() {
+        const getIcon = document.querySelector(".contact-icon")
+        var getCard = document.querySelector("#contact")
         
-//         getCard.style.display="none";
-//         document.querySelector("#contactClick").style.display="flex";
-//         console.log(getCard)
-//     },
+        getCard.style.display="none";
+        document.querySelector("#contactClick").style.display="flex";
+    },
     
-//     close() {
-//         document.querySelector("#contactClick").style.display="none";
-//         var getCard = document.querySelector("#contact")
-//         getCard.style.display="flex";
+    close() {
+        document.querySelector("#contactClick").style.display="none";
+        var getCard = document.querySelector("#contact")
+        getCard.style.display="flex";
 
-//     }
-// };
-
-
-// let screen = document.getElementById("screen");
-// let context = screen.getContext("2d");
-
-// let x = 150
-// let y = 300
-// let radius = 350
-// let begin = 0
-// let end = 2 * Math.PI
-
-// context.beginPath() //define um novo caminho
-
-// context.arc(x, y, radius, begin, end)
-
-// context.strokeStyle = "#212121";
-// context.fillStyle = "#fff";
-// context.fill()
-// /*
-// argumentos: 
-//     1º - distância da borda (horizontal);
-//     2º - distância da borda vertical;
-//     3º - raio;
-//     4º - ângulo que ele começa;
-//     5º - ângulo que ele termina.
-//     OBS: o começo e fim (4º e 5º) são medidas em randianos
-//  */
-
-// context.stroke() //diz que estamos de fato desenhando.
+    }
+};
